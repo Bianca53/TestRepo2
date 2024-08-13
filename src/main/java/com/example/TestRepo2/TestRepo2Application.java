edf02cbd12a7086b9eb1bf6870267b852f870f20
Bianca53 <bbia754@gmail.com>
2024-08-12 21:37:16 -0300

    initial commit

---
+++ src/main/java/com/example/TestRepo2/TestRepo2Application.java
@@ -0,0 +1,13 @@
+package com.example.TestRepo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestRepo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestRepo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TestRepo2/TestRepo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TestRepo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestRepo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
